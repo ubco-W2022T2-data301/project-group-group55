a639fba08d251965e36bb33ece5901b4ec238749
MataijaH <mataija@live.ca>
2023-03-20 20:30:43 +0000

    created function for cleaning & processing data

--- analysis/code/project_functions2.py
+++ analysis/code/project_functions2.py
@@ -12,6 +12,9 @@ def clean_and_process(csv_url):
         .dropna(subset = ['satisfaction','Customer Type','Type of Travel','Flight Distance','Departure Delay in Minutes', 'Arrival Delay in Minutes'])
         .reset_index(drop =True)
             )
+    DDoutliers = data['Departure Delay in Minutes'].mean() +(data['Departure Delay in Minutes'].std()*3)
+    ADoutliers = data['Arrival Delay in Minutes'].mean() +(data['Arrival Delay in Minutes'].std()*3)
+    FDoutliers = data['Flight Distance'].mean() +(data['Flight Distance'].std()*3)
     
     data1 = (data.assign(Total_Delay = (data['Departure Delay in Minutes']+data['Arrival Delay in Minutes']))
          .rename(columns={'Departure Delay in Minutes': 'Departure_Delay', 'Arrival Delay in Minutes' : 'Arrival_Delay',
